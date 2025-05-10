6e6063eb9aae1d3083c55bb833ce9d644e296e60
nicolevelozo <nicoleleonorvelozo@gmail.com>
2025-05-10 18:12:36 -0400

    agregamos script

---
+++ script.js
@@ -0,0 +1,32 @@
+const students=[];
+
+ document.getElementById("studentForm").addEventListener("submit",function (e){
+  e.preventDefault();
+
+  const name=document.getElementById("name").value.trim();
+  const lastName=document.getElementById("lastName").value.trim();
+  const grade=parseFloat(document.getElementById("grade").value);
+
+  if(!name || !lastName || isNaN(grade) || grade <1 || grade>7){
+    alert("Error al Ingresar los datos")
+    return  
+  }
+  
+  const student={name,lastName,grade}
+   students.push(student)
+   console.log(students)
+   addStudentToTable(student)
+
+   this.reset();
+ });
+
+ const tableBody=document.querySelector("#studentTable tbody");
+ function addStudentToTable(student){
+   const row=document.createElement("tr");
+   row.innerHTML=`
+   <td>${student.name}</td>
+    <td>${student.lastName}</td>
+     <td>${student.grade}</td>
+   `;
+tableBody.appendChild(row)
+ }
